baed86905d77008546464dc2ebd34c4a509be5e6
Robson Batista de Araujo <robsoncaliban@gmail.com>
2024-11-25 19:53:21 -0300

    initial commit

---
+++ src/main/java/com/dac/sanus_api/SanusApiApplication.java
@@ -0,0 +1,13 @@
+package com.dac.sanus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dac/sanus_api/SanusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dac.sanus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
