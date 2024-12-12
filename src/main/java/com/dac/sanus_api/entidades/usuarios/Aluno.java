9ad00f0bb259055582d746f2d89b84d5ce052872
Atos Alves <atosalvesqueiroz@gmail.com>
2024-12-11 23:57:00 -0300

    feat: Cria relacionamento com a entidade AgendamentoAula

--- src/main/java/com/dac/sanus_api/entidades/usuarios/Aluno.java
+++ src/main/java/com/dac/sanus_api/entidades/usuarios/Aluno.java
@@ -1,13 +1,17 @@
 package com.dac.sanus_api.entidades.usuarios;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
+
+import com.dac.sanus_api.entidades.AgendamentoAula;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -16,19 +20,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name = "TB_ALUNO" )
-public class Aluno implements Serializable{
+@Table(name = "TB_ALUNO")
+public class Aluno implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     private UUID id;
     @OneToOne
     @MapsId
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
-    //TODO: matricula vai ser gerada automaticamente?
+    // TODO: matricula vai ser gerada automaticamente?
     @Column(unique = true)
     private String matricula;
+
+    @OneToMany(mappedBy = "aluno")
+    private List<AgendamentoAula> agendamentos;
 
     public Aluno(Usuario usuario, String matricula) {
         this.usuario = usuario;
