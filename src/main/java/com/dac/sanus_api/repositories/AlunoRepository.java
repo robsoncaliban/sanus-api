5168c7674e10f5713398a8a3630a6fb29800061a
robsoncaliban <robsoncaliban@gmail.com>
2025-02-24 21:03:46 -0300

    refactor: Remove busca por aluno ativo

--- src/main/java/com/dac/sanus_api/repositories/AlunoRepository.java
+++ src/main/java/com/dac/sanus_api/repositories/AlunoRepository.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.dac.sanus_api.entidades.usuarios.Aluno;
-import java.util.List;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
-
-    List<Aluno> findByAtivo(boolean ativo); 
 
     @Query("SELECT MAX(a.matricula) FROM Aluno a WHERE a.matricula LIKE :ano%")
     String findUltimaMatriculaPorAno(String ano);
