d0375202f1867de142acb6f7cf18d072c40c91e2
robsoncaliban <robsoncaliban@gmail.com>
2025-02-24 21:02:40 -0300

    feat: Adiciona query personalizada no AlunoRepository para buscar ultima matricula por ano

--- src/main/java/com/dac/sanus_api/repositories/AlunoRepository.java
+++ src/main/java/com/dac/sanus_api/repositories/AlunoRepository.java
@@ -1,12 +1,15 @@
 package com.dac.sanus_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dac.sanus_api.entidades.usuarios.Aluno;
 import java.util.List;
 
-
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
 
     List<Aluno> findByAtivo(boolean ativo); 
+
+    @Query("SELECT MAX(a.matricula) FROM Aluno a WHERE a.matricula LIKE :ano%")
+    String findUltimaMatriculaPorAno(String ano);
 }
