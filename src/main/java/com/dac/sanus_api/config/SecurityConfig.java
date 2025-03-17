ad684f245a4673114d8e27b1f54d6e4b90018fa2
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-16 23:42:57 -0300

    fix: Ajusta configuração de filtros de segurança para suporte a autenticação JWT personalizada

--- src/main/java/com/dac/sanus_api/config/SecurityConfig.java
+++ src/main/java/com/dac/sanus_api/config/SecurityConfig.java
@@ -9,22 +9,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.dac.sanus_api.services.UsuarioService;
+import com.dac.sanus_api.converter.CustomJwtAuthenticationConverter;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${jwt.public.key}")
@@ -32,6 +36,26 @@ public class SecurityConfig {
 
     @Value("${jwt.private.key}")
     private RSAPrivateKey privateKey;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomJwtAuthenticationConverter());
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE).hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/alunos/planos").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/alunos").hasAnyAuthority("ADMIN", "PROFESSOR")
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -48,27 +72,6 @@ public class SecurityConfig {
         JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
         var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
-    }
-
-    @Bean
-    DaoAuthenticationProvider authenticationProvider(UsuarioService userService) {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userService); // set the custom user details service
-        auth.setPasswordEncoder(passwordEncoder()); // set the password encoder - bcrypt
-        return auth;
-    }
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/alunos").hasAnyRole("ADMIN", "PROFESSOR")
-                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PUT,"/alunos/planos").hasRole("ADMIN")
-                .anyRequest().authenticated());
-            return http.build();
     }
 
 }
