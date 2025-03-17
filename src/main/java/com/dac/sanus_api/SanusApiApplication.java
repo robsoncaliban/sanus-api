2c6797cc113b5007ec236a1b21e441f4973ad7ca
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-16 23:34:09 -0300

    refactor: Remove exclusão da configuração de segurança na aplicação

--- src/main/java/com/dac/sanus_api/SanusApiApplication.java
+++ src/main/java/com/dac/sanus_api/SanusApiApplication.java
@@ -2,9 +2,8 @@ package com.dac.sanus_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class SanusApiApplication {
 
 	public static void main(String[] args) {
